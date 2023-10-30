fbeb4a143681cd0568ddd4d34d98217641ad6a9b
Yasv#20041 <yasvathika.21it@kongu.edu>
2023-10-30 22:37:41 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://mern-house-api.onrender.com',
         secure: false,
       },
     },
